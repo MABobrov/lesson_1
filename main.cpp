d6e7e4a6df2053f050592ff58de01a2ad2844ea0
Natalia Kryazheva <nataliakryazheva@MacBook-Air-Natalia.local>
2022-11-12 22:21:04 +0300

    вычисляет квадратный корень через D

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+//вычисляет квадратный корень через D
+ int main() {float a,b,c;
+   printf("a=");scanf("%f",&a);
+   printf("b=");scanf("%f",&b);
+   printf("c=");scanf("%f",&c);
+  if(a!=0){float d=b*b-4*a*c;
+  if(d>0){
+      float x1=(-b-sqrt(d))/(2*a); float x2=(-b+sqrt(d))/(2*a);
+      printf("X1=%f" "X2=%f",x1,x2);}
+   else{
+       if(d==0){
+           float x=(-b)/(2*a); printf("X=%f",x);}
+    else{printf("korney net,D<0");}}}
+    else{float x=-c/b; printf("X=%f",x);}
+return 0;}
